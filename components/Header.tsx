f2909732b51e77d7273a68249d92ef95e17db0bd
pa-rang <appdler96@kaist.ac.kr>
2021-04-25 16:26:24 +0900

    feat: add dark-mode-toggle to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { colors } from "../constants/colors";
+
+const Styled = {
+  Root: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 40px;
+    height: 72px;
+  `,
+
+  DarkModeToggle: styled.div`
+    padding: 8px 16px;
+    border-radius: 4px;
+    color: ${colors.gray90};
+    background-color: ${colors.gray20};
+  `,
+};
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Styled.Root>
+      <div />
+      <Styled.DarkModeToggle>Light</Styled.DarkModeToggle>
+    </Styled.Root>
+  );
 }
 
 export default Header;
