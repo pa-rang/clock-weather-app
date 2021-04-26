3dcaf32ee3dfe69c4a24e1de03f594c42f2436d0
pa-rang <appdler96@kaist.ac.kr>
2021-04-26 19:11:33 +0900

    feat: show seoul weather data

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -6,8 +6,16 @@ function Weather() {
   const { data, error } = useSWR("/weather", () => getCurrentWeather("seoul"));
 
   console.log(`data`, data);
+  const ABS_ZERO = 273.15; // 이 API에서는 온도에 절대영도를 사용함
+  const temperatures = (data.main.temp - ABS_ZERO).toFixed(2);
 
-  return <div></div>;
+  return (
+    <div>
+      <div>{data.name}</div>
+      <div>{temperatures}°C</div>
+      <div>{data.weather[0].main}</div>
+    </div>
+  );
 }
 
 export default Weather;
