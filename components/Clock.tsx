a9c0589b8d70ae1c7a470b0fd89556d5499f7241
pa-rang <appdler96@kaist.ac.kr>
2021-05-07 19:06:45 +0900

    style(clock): fix clock style

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -6,12 +6,14 @@ import { ThemeType } from "../lib/types";
 import Box from "./common/Box";
 
 const Styled = {
-  Root: styled.div`
+  Root: styled.div<{ theme: ThemeType }>`
     width: 200px;
     height: 200px;
     border-radius: 50%;
-    background-color: ${colors.gray10};
-    border: 1px solid ${colors.gray40};
+    background-color: ${({ theme }) =>
+      theme === "dark" ? colors.gray90 : colors.gray10};
+    border: 3px solid
+      ${({ theme }) => (theme === "dark" ? colors.gray10 : colors.gray70)};
     & > div {
       position: relative;
       border-radius: 2px;
@@ -19,30 +21,33 @@ const Styled = {
     }
   `,
 
-  HourHand: styled.div<{ hourDegree: number }>`
+  HourHand: styled.div<{ hourDegree: number; theme: ThemeType }>`
     top: 100px;
     left: 40px;
     width: 60px;
     height: 4px;
-    background-color: ${colors.gray50};
+    background-color: ${({ theme }) =>
+      theme === "dark" ? colors.gray10 : colors.gray80};
     transform: ${({ hourDegree }) => `rotate(${hourDegree}deg)`};
   `,
 
-  MunuteHand: styled.div<{ minuteDegree: number }>`
+  MunuteHand: styled.div<{ minuteDegree: number; theme: ThemeType }>`
     top: 98px;
     left: 20px;
     width: 80px;
     height: 3px;
-    background-color: ${colors.gray70};
+    background-color: ${({ theme }) =>
+      theme === "dark" ? colors.gray10 : colors.gray80};
     transform: ${({ minuteDegree }) => `rotate(${minuteDegree}deg)`};
   `,
 
-  SecondHand: styled.div<{ secondDegree: number }>`
+  SecondHand: styled.div<{ secondDegree: number; theme: ThemeType }>`
     top: 96px;
     left: 10px;
     width: 90px;
     height: 2px;
-    background-color: ${colors.green50};
+    background-color: ${({ theme }) =>
+      theme === "dark" ? colors.gray10 : colors.gray80};
     transform: ${({ secondDegree }) => `rotate(${secondDegree}deg)`};
   `,
 };
@@ -66,10 +71,10 @@ function Clock({ theme }: Props): ReactElement {
 
   return (
     <Box title="시계" theme={theme} center>
-      <Styled.Root>
-        <Styled.HourHand hourDegree={hourDegree} />
-        <Styled.MunuteHand minuteDegree={minuteDegree} />
-        <Styled.SecondHand secondDegree={secondDegree} />
+      <Styled.Root theme={theme}>
+        <Styled.HourHand hourDegree={hourDegree} theme={theme} />
+        <Styled.MunuteHand minuteDegree={minuteDegree} theme={theme} />
+        <Styled.SecondHand secondDegree={secondDegree} theme={theme} />
       </Styled.Root>
     </Box>
   );
