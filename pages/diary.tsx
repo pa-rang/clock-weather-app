cc293f264a9fdfbf63693e47c5a2b6c3da50ffff
pa-rang <appdler96@kaist.ac.kr>
2021-05-20 00:30:49 +0900

    feat(Diary): add months selector

--- pages/diary.tsx
+++ pages/diary.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from "react";
 import Box from "../components/common/Box";
 import Header from "../components/common/Header";
 import styled from "@emotion/styled";
+import { months } from "../lib/constants/months";
+import { colors } from "../lib/constants/colors";
 
 const Styled = {
   BoxWrapper: styled.div`
@@ -13,6 +15,21 @@ const Styled = {
     width: 100%;
     border-radius: 12px;
     margin-bottom: 16px;
+  `,
+
+  MonthSelector: styled.div`
+    display: flex;
+    overflow-x: scroll;
+    height: 80px;
+    align-items: center;
+    cursor: pointer;
+
+    & > div {
+      padding: 8px 16px;
+      border-radius: 8px;
+      margin-right: 16px;
+      background-color: ${colors.gray80};
+    }
   `,
 };
 
@@ -52,6 +69,11 @@ function Diary(): ReactElement {
     <>
       <Header />
       <Styled.BoxWrapper>
+        <Styled.MonthSelector>
+          {months.map((month) => (
+            <div>{month}</div>
+          ))}
+        </Styled.MonthSelector>
         {diaryData.map(() => (
           <Box title="오늘의 일기">
             <Styled.DiaryImage
