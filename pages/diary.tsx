16a64d7e9f5cc29207425fce26f192c4fa8ab7e9
pa-rang <appdler96@kaist.ac.kr>
2021-05-19 23:55:51 +0900

    feat(Diary): add diary card

--- pages/diary.tsx
+++ pages/diary.tsx
@@ -8,16 +8,59 @@ const Styled = {
     margin: 0 auto;
     max-width: 360px;
   `,
+
+  DiaryImage: styled.img`
+    width: 100%;
+    border-radius: 12px;
+    margin-bottom: 16px;
+  `,
 };
+
+const IMG_URL =
+  "https://parang-me.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%B5%E1%84%87%E1%85%A1.png";
+
+const diaryData = [
+  {
+    title: "오늘의 일기",
+    src: IMG_URL,
+    contents: "오늘은 맛있는걸 먹었다. 출근하기 싫다",
+  },
+  {
+    title: "오늘의 일기",
+    src: IMG_URL,
+    contents: "오늘은 맛있는걸 먹었다. 출근하기 싫다",
+  },
+  {
+    title: "오늘의 일기",
+    src: IMG_URL,
+    contents: "오늘은 맛있는걸 먹었다. 출근하기 싫다",
+  },
+  {
+    title: "오늘의 일기",
+    src: IMG_URL,
+    contents: "오늘은 맛있는걸 먹었다. 출근하기 싫다",
+  },
+  {
+    title: "오늘의 일기",
+    src: IMG_URL,
+    contents: "오늘은 맛있는걸 먹었다. 출근하기 싫다",
+  },
+];
 
 function Diary(): ReactElement {
   return (
     <>
       <Header />
       <Styled.BoxWrapper>
-        <Box title="오늘의 일기">
-          <div>Diary</div>
-        </Box>
+        {diaryData.map(() => (
+          <Box title="오늘의 일기">
+            <Styled.DiaryImage
+              src="https://parang-me.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%B5%E1%84%87%E1%85%A1.png"
+              alt="image"
+            />
+            <div>오늘은 맛있는걸 먹었다. 출근하기 싫다</div>
+          </Box>
+        ))}
       </Styled.BoxWrapper>
     </>
   );
